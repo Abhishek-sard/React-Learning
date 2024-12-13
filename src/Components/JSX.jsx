af9719e60be48e75f5dbac52c6afb6cf6276e3e0
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-12-13 21:17:30 +0545

    push

---
+++ src/Components/JSX.jsx
@@ -0,0 +1,2 @@
+//jsx is the syntax extension that allows you to write HTMl-like code inside javascript.HTMl
+const element = <h1>Hello, word!</h1>;
---
+++ src/Components/components.jsx
@@ -0,0 +1,6 @@
+//React is based on components, which are the building blocks of the UI.
+//Types: functional components and class components
+
+function MyComponent(){
+    return<h2>Welcome to my React component!</h2>
+}
